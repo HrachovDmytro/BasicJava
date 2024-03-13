7b6114aab09b07a1ec7227c1f26580809ae908f0
HrachovDmytro <hrachevdmitry@gmail.com>
2024-03-13 12:45:36 +0200

    Created LR_2 branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
         //нульва лаб робота
-
+        System.out.println("Створенна гілка для лабораторної роботи 2");
         System.out.println("Hello World!");
     }
 }
